38f7e7f0e1552bce8824ea68cf6fc5a82622dc57
chwasiq0569 <wasiqabdullah222@gmail.com>
2021-06-05 12:57:47 -0700

    Cleared the App & revamped the components

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -1,75 +1,35 @@
-import * as React from "react";
-import { AiOutlineHome } from "react-icons/ai";
-import { AiOutlineBell } from "react-icons/ai";
-import { MdChatBubbleOutline } from "react-icons/md";
-import { MdStarBorder } from "react-icons/md";
-import { BsBookmarks } from "react-icons/bs";
-import { BsBell } from "react-icons/bs";
-import Image from "next/image";
+import React from "react";
 import styles from "./header.module.css";
+// import LogoImg from "/assets/icons/brandicon.svg";
+import { AiFillHome } from "react-icons/ai";
+import { MdBookmark } from "react-icons/md";
+import { MdNotifications } from "react-icons/md";
+import { RiMessage2Fill } from "react-icons/ri";
+import { IoMdArrowDropdownCircle } from "react-icons/io";
+import { IoMdVideocam } from "react-icons/io";
+
 export interface HeaderProps {}
 
 const Header: React.FC<HeaderProps> = () => {
-  const iconStyles = {
-    color: "#b9b9b9",
-    fontSize: "2rem",
-    cursor: "pointer",
-    backgroundColor: "none",
-    padding: "0.425rem",
-    borderRadius: "0.25rem",
-    marginBottom: "0.25rem",
-  };
-
   return (
-    <div
-      className={
-        styles.headerContainer +
-        " sm: w-full fixed bottom-0 md: lg:relative max-w-screen-xl bg-brand-primary h-12 flex flex-row justify-between px-2"
-      }
-    >
-      <div
-        className={
-          styles.HeaderLeftSide + " w-12 h-12 flex justify-center items-center"
-        }
-      >
-        <div
-          className={
-            styles.brandIconContainer +
-            " w-12 h-12 flex justify-center items-center"
-          }
-        >
-          <Image
-            src="/assets/icons/brandicon.svg"
-            alt="BrandIcon"
-            width={"25rem"}
-            height={"25rem"}
-          />
+    <div className={styles.headerContainer}>
+      <div className={styles.leftSide}>
+        <div className={styles.logoContainer}>
+          <img src="/assets/icons/brandicon.svg" alt="Logo" />
         </div>
       </div>
-      <div
-        className={
-          styles.menuContainer +
-          " w-full md:w-2/4 h-12 rounded-b-xl bg-brand-secondary flex flex-row justify-between items-center lg:w-2/4 "
-        }
-      >
-        <AiOutlineHome style={iconStyles} />
-        <BsBell style={iconStyles} />
-        <MdChatBubbleOutline style={iconStyles} />
-        <BsBookmarks style={iconStyles} />
+      <div className={styles.middleSide}>
+        <div className={styles.iconsContainer}>
+          <AiFillHome className={styles.middle_iconStyles} />
+          <IoMdVideocam className={styles.middle_iconStyles} />
+          <MdBookmark className={styles.middle_iconStyles} />
+          <RiMessage2Fill className={styles.middle_iconStyles} />
+        </div>
       </div>
-      <div className="h-12 flex justify-center items-center">
-        <div
-          className={
-            styles.profileImageContainer +
-            "h-10 flex justify-center items-center"
-          }
-        >
-          <p className={styles.userName}>chwasiq0569</p>
-          <img
-            className={styles.placeholderImage}
-            src="/assets/icons/l60Hf.png"
-            alt="User Placeholder Image"
-          />
+      <div className={styles.rightSide}>
+        <div className={styles.iconsContainer}>
+          <MdNotifications className={styles.end_iconStyles} />
+          <IoMdArrowDropdownCircle className={styles.end_iconStyles} />
         </div>
       </div>
     </div>
---
+++ next.config.js
@@ -0,0 +1,20 @@
+// module.exports = {
+//   async rewrites() {
+//     [
+//       {
+//         source: "/api/:slug*",
+//         destination: `http://localhost:8080/api/:slug*`,
+//       },
+//     ];
+//   },
+// };
+// module.exports = {
+//   async rewrites() {
+//     return [
+//       {
+//         source: '/feedback',
+//         destination: '/feedback/general',
+//       },
+//     ]
+//   },
+// }
